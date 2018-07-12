8941f94a2ddaeaf1add0bbf93a39b2f20ab55a66
andrew <andrew@debra2.debra2.local>
2018-07-12 01:37:51 -0500

    rebuild web app for 15 servos, eliminate sencha build process

--- pca96850-servo-webtest/buildraspi.sh
+++ pca96850-servo-webtest/buildraspi.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+cd ~/go/src/github.com/RyAndrew/raspi-embd/pca96850-servo-webtest/
 export GOARCH=arm
 go build pca9685-servo-webtest.go
 
 scp -i ~/pikey pca9685-servo-webtest pi@10.88.0.115:~/
+
+#scp -r -i ~/pikey webroot pi@10.88.0.115:~/webroot
